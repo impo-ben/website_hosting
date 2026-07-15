0b0603be854691009a5c079762ded0531e3a8a75
impo-ben <benjaminpaulp42@gmail.com>
2026-07-15 18:39:46 +0530

    deployment bug

--- CompanyHub/config/settings.py
+++ CompanyHub/config/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+import os
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-development-only")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
